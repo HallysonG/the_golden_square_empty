4c9a4e1c539a412cf0b91c3cb75b6cdd728884ed
Hallyson <hallyson.garcia@gmail.com>
2023-04-24 17:03:48 +0100

    testing for errors challenge

---
+++ tests/test_password_checker.py
@@ -0,0 +1,16 @@
+import pytest
+from lib.password_checker import *
+
+def test_password_checker_valid():
+    passwordchecker = PasswordChecker()
+    result = passwordchecker.check("12345678")
+    assert result == True
+
+def test_password_check_error():
+    passwordchecker = PasswordChecker()
+    with pytest.raises(Exception) as e:
+        result = passwordchecker.check("1234567")
+    error_message = str(e.value)
+    assert error_message == "Invalid password, must be 8+ characters."
+    
+    
