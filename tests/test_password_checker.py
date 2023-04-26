642d3189a1e884ed045d7935e05d13611a7bbc56
Hallyson <hallyson.garcia@gmail.com>
2023-04-26 15:24:14 +0100

    Test Drive Challenge

--- tests/test_password_checker.py
+++ tests/test_password_checker.py
@@ -12,5 +12,5 @@ def test_password_check_error():
         result = passwordchecker.check("1234567")
     error_message = str(e.value)
     assert error_message == "Invalid password, must be 8+ characters."
-    
+
     
