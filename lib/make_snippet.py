c03f2f51d65cf017d28bbde9e5ff74ed5a6b9e4b
Mumtaz <mumtazob@gmail.com>
2023-04-26 14:48:27 +0100

    exercise completed

---
+++ lib/make_snippet.py
@@ -0,0 +1,8 @@
+def make_snippet(string):
+    if len(string.split()) < 6:
+        return string
+    else:
+        five = string.split()[:5]
+        rightfive= " ".join(five)
+        return f"{rightfive}..."
+
